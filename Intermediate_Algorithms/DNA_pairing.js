8a119c5bc85dbffbebeb7f5d06df3d33156c85cf
Juan José <66780299+JuanJosePG@users.noreply.github.com>
2020-10-21 17:00:19 +0200

    Create DNA_pairing.js

---
+++ Intermediate_Algorithms/DNA_pairing.js
@@ -0,0 +1,20 @@
+function pairElement(str) {
+  let arr=[];
+  for(let i=0;i<str.length;i++){
+    switch(str[i]){
+      case "A":
+        arr.push(["A", "T"]);
+        break;
+      case "T":
+        arr.push(["T", "A"]);
+        break;
+      case "C":
+        arr.push(["C", "G"]);
+        break;
+      case "G":
+        arr.push(["G", "C"]);
+        break;
+    }
+  }
+  return arr;
+}
