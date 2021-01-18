9dde9588b02059ce2e6347e745118abfe0d0de54
jjpg__code <jjpg.code@gmail.com>
2021-01-18 17:13:01 +0100

    Create es6-translation.js

---
+++ More/es6-translation.js
@@ -0,0 +1,27 @@
+// var increment = function(n){
+//   return n+1;
+// }
+
+var increment = n => {
+  return n+1;
+}
+
+// var square = function(n){
+//   return n*n;
+// }
+
+var square = n => {
+  return n*n;
+}
+
+
+// var doMathSoIDontHaveTo = function(n,func){
+//   return func(n);
+// }
+
+var doMathSoIDontHaveTo = (n,func) => {
+  return func(n);
+}
+
+doMathSoIDontHaveTo(5, square); //25
+doMathSoIDontHaveTo(4, increment); //5
