33ea4bc8be7397e055877ca626c8d03e6fd054b6
jjpg__code <jjpg.code@gmail.com>
2021-01-18 17:09:33 +0100

    Create destructured-data-structure.js

---
+++ More/destructured-data-structure.js
@@ -0,0 +1,15 @@
+var suspects = [
+  {
+    name: 'Rusty',
+    color: 'orange',
+  },
+  {
+    name: 'Miss Scarlet',
+    color: 'red',
+  },
+];
+
+
+const [a,b]=[suspects[0].color, suspects[1].color]
+console.log(a) //'orange'
+console.log(b) //'red'
